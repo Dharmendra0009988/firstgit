48d17530df79edbf98eeb87d0d0721995fcb328d
Dharmendra0009988 <120949276+Dharmendra0009988@users.noreply.github.com>
2023-01-27 22:43:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,141 @@
+// function myFunction() {
+//     document.getElementById("box1").style.color = "red";
+//     document.getElementById("box4").style.rotate="70deg";
+// };
+// let keyFrames = "@keyframes rotation {\
+//     from {\
+//       transform: rotate(0deg);\
+//     }\
+//     to {\
+//       transform: rotate(359deg);\
+//     }\
+//   }"
+// function anotherFn() {
+//     document.getElementById("box6").style.animation = "rotation 4s infinite liner"+`${keyFrames}`; 
+//     document.getElementById("box6").style = `${keyFrames}`;
+// }
+function box1() {
+    document.getElementById("box6").style.backgroundColor = "green";
+    document.getElementById("box4").style.backgroundColor = "violet";
+    document.getElementById("box2").style.backgroundColor = "gold";
+    document.getElementById("box3").style.backgroundColor = "red";
+    document.getElementById("box1").style.backgroundColor = "pink";
+    document.getElementById("box5").style.backgroundColor = "orange";
+    document.getElementById("box4").style.color = "green";
+    document.getElementById("box6").style.color = "violet";
+    document.getElementById("box5").style.color = "black";
+    document.getElementById("box3").style.color = "purple";
+    document.getElementById("box1").style.color = "orange";
+    document.getElementById("box2").style.color = "white";
+    document.getElementById("box6").innerHTML ="Hey! I am Here...";
+    document.getElementById("box1").innerHTML = "";
+    document.getElementById("box2").innerHTML = "";
+    document.getElementById("box3").innerHTML = "";
+    document.getElementById("box4").innerHTML = "";
+    document.getElementById("box5").innerHTML = "";
+}
+function box2() {
+    document.getElementById("box5").style.backgroundColor = "green";
+    document.getElementById("box4").style.backgroundColor = "violet";
+    document.getElementById("box6").style.backgroundColor = "gold";
+    document.getElementById("box2").style.backgroundColor = "red";
+    document.getElementById("box1").style.backgroundColor = "pink";
+    document.getElementById("box3").style.backgroundColor = "orange";
+    document.getElementById("box6").style.color = "black";
+    document.getElementById("box4").style.color = "skyblue";
+    document.getElementById("box5").style.color = "yellow";
+    document.getElementById("box3").style.color = "white";
+    document.getElementById("box1").style.color = "tomato";
+    document.getElementById("box2").style.color = "blue";
+    document.getElementById("box5").innerHTML ="Hey! I am Here..."
+    document.getElementById("box1").innerHTML = "";
+    document.getElementById("box2").innerHTML = "";
+    document.getElementById("box3").innerHTML = "";
+    document.getElementById("box4").innerHTML = "";
+    document.getElementById("box6").innerHTML = "";
+
+}
+function box3() {
+    document.getElementById("box1").style.backgroundColor = "green";
+    document.getElementById("box5").style.backgroundColor = "violet";
+    document.getElementById("box2").style.backgroundColor = "gold";
+    document.getElementById("box3").style.backgroundColor = "red";
+    document.getElementById("box4").style.backgroundColor = "pink";
+    document.getElementById("box6").style.backgroundColor = "orange";
+    document.getElementById("box4").style.color = "black";
+    document.getElementById("box5").style.color = "skyblue";
+    document.getElementById("box6").style.color = "yellow";
+    document.getElementById("box1").style.color = "white";
+    document.getElementById("box2").style.color = "tomato";
+    document.getElementById("box3").style.color = "blue";
+    document.getElementById("box1").innerHTML ="Hey! I am Here..."
+    document.getElementById("box6").innerHTML = "";
+    document.getElementById("box2").innerHTML = "";
+    document.getElementById("box3").innerHTML = "";
+    document.getElementById("box4").innerHTML = "";
+    document.getElementById("box5").innerHTML = "";
+
+}
+function box4() {
+    document.getElementById("box2").style.backgroundColor = "green";
+    document.getElementById("box5").style.backgroundColor = "violet";
+    document.getElementById("box3").style.backgroundColor = "gold";
+    document.getElementById("box4").style.backgroundColor = "red";
+    document.getElementById("box1").style.backgroundColor = "orange";
+    document.getElementById("box6").style.backgroundColor = "pink";
+    document.getElementById("box1").style.color = "black";
+    document.getElementById("box2").style.color = "skyblue";
+    document.getElementById("box3").style.color = "yellow";
+    document.getElementById("box3").style.color = "white";
+    document.getElementById("box5").style.color = "tomato";
+    document.getElementById("box6").style.color = "blue";
+    document.getElementById("box2").innerHTML ="Hey! I am Here..."
+    document.getElementById("box1").innerHTML = "";
+    document.getElementById("box6").innerHTML = "";
+    document.getElementById("box3").innerHTML = "";
+    document.getElementById("box4").innerHTML = "";
+    document.getElementById("box5").innerHTML = "";
+
+}
+function box5() {
+    document.getElementById("box3").style.backgroundColor = "green";
+    document.getElementById("box2").style.backgroundColor = "violet";
+    document.getElementById("box6").style.backgroundColor = "gold";
+    document.getElementById("box5").style.backgroundColor = "red";
+    document.getElementById("box1").style.backgroundColor = "orange";
+    document.getElementById("box4").style.backgroundColor = "pink";
+    document.getElementById("box3").style.color = "black";
+    document.getElementById("box1").style.color = "skyblue";
+    document.getElementById("box6").style.color = "yellow";
+    document.getElementById("box2").style.color = "white";
+    document.getElementById("box4").style.color = "tomato";
+    document.getElementById("box5").style.color = "blue";
+    document.getElementById("box3").innerHTML ="Hey! I am Here..."
+    document.getElementById("box1").innerHTML = "";
+    document.getElementById("box2").innerHTML = "";
+    document.getElementById("box6").innerHTML = "";
+    document.getElementById("box4").innerHTML = "";
+    document.getElementById("box5").innerHTML = "";
+
+}
+function box6() {
+    document.getElementById("box4").style.backgroundColor = "green";
+    document.getElementById("box2").style.backgroundColor = "violet";
+    document.getElementById("box1").style.backgroundColor = "gold";
+    document.getElementById("box6").style.backgroundColor = "red";
+    document.getElementById("box5").style.backgroundColor = "pink";
+    document.getElementById("box3").style.backgroundColor = "orange";
+    document.getElementById("box2").style.color = "black";
+    document.getElementById("box1").style.color = "skyblue";
+    document.getElementById("box3").style.color = "yellow";
+    document.getElementById("box6").style.color = "white";
+    document.getElementById("box5").style.color = "tomato";
+    document.getElementById("box4").style.color = "blue";
+    document.getElementById("box4").innerHTML ="Hey! I am Here..."
+    document.getElementById("box1").innerHTML = "";
+    document.getElementById("box2").innerHTML = "";
+    document.getElementById("box3").innerHTML = "";
+    document.getElementById("box6").innerHTML = "";
+    document.getElementById("box5").innerHTML = "";
+}
+
